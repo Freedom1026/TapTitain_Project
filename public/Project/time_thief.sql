8e493cb7e254c4ee8b01735fb084a8634c815986
user <ooo80422@gmail.com>
2020-04-26 14:31:51 +0800

    mysql-change

--- public/Project/time_thief.sql
+++ public/Project/time_thief.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2020-04-14 10:20:12
--- 伺服器版本： 10.4.11-MariaDB
--- PHP 版本： 7.4.4
+-- 產生時間： 
+-- 伺服器版本： 10.4.8-MariaDB
+-- PHP 版本： 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -81,6 +82,56 @@ INSERT INTO `address` (`uid`, `country`, `area`, `detail`) VALUES
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `cards_list`
+--
+
+DROP TABLE IF EXISTS `cards_list`;
+CREATE TABLE IF NOT EXISTS `cards_list` (
+  `diamond_card` varchar(36) NOT NULL,
+  `password` varchar(24) NOT NULL,
+  `state` int(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`diamond_card`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 傾印資料表的資料 `cards_list`
+--
+
+INSERT INTO `cards_list` (`diamond_card`, `password`, `state`) VALUES
+('015a96c6-3586-4fe0-9d18-37a8df2edc5b', '5ea3c15633a9809fa2e8de4d', 0),
+('09d05517-fde4-4505-b5ed-7fa6b2dfed01', '5ea3c1562cb4fc244b76bf39', 1),
+('1c7f5128-f406-4565-bb87-c69b5b92a893', '5ea3c156d4a4bea6ef4fcf61', 1),
+('1e6833f4-7568-4ad4-a39f-ef7f16c2eeda', '5ea3c156909169f0a868de37', 1),
+('239aea7e-fd82-47dd-b2bb-1f6e651af07a', '5ea3c156e838b58e22535ea4', 1),
+('2b7cd161-bf3e-47d7-af49-5fdbbf8d4cc1', '5ea3c156637b61491bb4a0e6', 1),
+('3cd1e274-ecbd-4175-a354-40b7cd7d33f1', '5ea3c1562d50030191a1e88c', 1),
+('4b3dbb89-3c6a-436c-b720-cbc4720a4373', '5ea3c156a40827cc37b19442', 1),
+('4c1a5bba-a602-43ab-bcdc-01e83323a21c', '5ea3c156f5e95150757a83c8', 1),
+('5b5d98ae-dc97-44a6-ab24-298a131cc00d', '5ea3c1564362e0bbe8baeb19', 1),
+('5f18e9b4-bb3d-4cfe-80c4-1e48ab8704a4', '5ea3c1566a90a1079812ea5b', 1),
+('65eec8ea-4380-4a16-a631-2e0252b606ef', '5ea3c156fddb6b07ec4ca406', 1),
+('6bf0f899-29d3-4708-8ab6-acc1b7154e71', '5ea3c156b6de50dadbe7f641', 1),
+('7814e1bb-c0e2-4198-bb65-8e358885c669', '5ea3c1569e824ebd9ea792b5', 1),
+('78dc3632-013d-4aec-b475-6d04f13e7a7c', '5ea3c156568fac4c3fcf3dd4', 1),
+('7b257116-8d4f-4cac-a90c-b5ee18b34f9e', '5ea3c1565931596398bcc0c3', 1),
+('7d9d5438-23c2-470a-bdb7-54577737f77a', '5ea3c1561ddd3b2d25ed2269', 1),
+('7deb2cfb-a058-4ef0-8f1e-02210a92ecdb', '5ea3c1566d45254d066d1fbf', 1),
+('82b18136-749f-4531-bbcd-88fd43b3ab09', '5ea3c1560848837759af02ac', 1),
+('8c7e9e39-7d39-43b0-9fcc-f787118ff44e', '5ea3c1561842295092baf8a0', 1),
+('9491b812-10bb-46be-a4bd-9061b1e58728', '5ea3c156f371564952ea4158', 1),
+('a57774d2-a0cd-464b-9010-2a3c888374ae', '5ea3c156394ee16d75b03e46', 1),
+('b680862e-26fe-4903-a95e-13f0c094ca4c', '5ea3c156cb3ffcb758fab1ba', 1),
+('c314572f-5e76-4340-924d-d95604c19a8a', '5ea3c15658874266635700a3', 1),
+('c6a48509-733f-4420-b6b8-37ff374cfd95', '5ea3c1564f553cf924f549ac', 1),
+('dca019d5-7474-4e8c-aafc-7aac2eaad808', '5ea3c156e96f78d495c6a371', 1),
+('ea5b2909-5feb-49fc-aff6-b6f8b80e48c5', '5ea3c15633fad7a653d5b5e2', 1),
+('f6e1958e-bb31-4767-a0a5-c7ddd948b2c5', '5ea3c156235428e53bfdbbb9', 1),
+('f795ae05-c321-47f2-9742-863f4b00cd00', '5ea3c1565009fd44ecc3009b', 1),
+('fd375dc2-5369-49a3-a280-0dd0d6f267b6', '5ea3c156c22231429dd5ded7', 1);
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `contact`
 --
 
@@ -122,7 +173,30 @@ CREATE TABLE IF NOT EXISTS `creatureskill` (
 --
 
 INSERT INTO `creatureskill` (`uid`, `yellow`, `purple`, `blue`, `orange`) VALUES
-(21, 0, 0, 0, 0);
+(21, 4, 3, 3, 0);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `diamond`
+--
+
+DROP TABLE IF EXISTS `diamond`;
+CREATE TABLE IF NOT EXISTS `diamond` (
+  `uid` int(10) NOT NULL,
+  `modified_date` int(10) NOT NULL DEFAULT current_timestamp(),
+  `amount` int(10) NOT NULL,
+  `diamond_card` varchar(36) NOT NULL,
+  `the_rest` int(10) NOT NULL,
+  PRIMARY KEY (`diamond_card`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 傾印資料表的資料 `diamond`
+--
+
+INSERT INTO `diamond` (`uid`, `modified_date`, `amount`, `diamond_card`, `the_rest`) VALUES
+(21, 1587791843, 200, '015a96c6-3586-4fe0-9d18-37a8df2edc5b', 200);
 
 -- --------------------------------------------------------
 
@@ -191,7 +265,7 @@ CREATE TABLE IF NOT EXISTS `myself` (
 --
 
 INSERT INTO `myself` (`uid`, `lv`, `stage`, `coin`, `diamond`, `sk_A`, `sk_B`, `achievement`) VALUES
-(21, 3, 8, 0, 0, 1, 1, 0);
+(21, 38, 18, 207, 20, 3, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -209,6 +283,7 @@ CREATE TABLE IF NOT EXISTS `order_list` (
   `item_id` int(5) NOT NULL,
   `product_id` varchar(10) NOT NULL,
   `product_price` int(5) NOT NULL,
+  `product_n` int(10) NOT NULL,
   PRIMARY KEY (`no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -220,7 +295,6 @@ CREATE TABLE IF NOT EXISTS `order_list` (
 
 DROP TABLE IF EXISTS `payment`;
 CREATE TABLE IF NOT EXISTS `payment` (
-  `pay_date` int(10) NOT NULL,
   `method_pay` varchar(10) NOT NULL,
   `order_id` int(10) NOT NULL,
   `amount` int(10) NOT NULL,
